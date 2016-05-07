325cad5be1e0ac7dc31ffc0848a9322c4151665c
Arturo Rivas <arturo.rivas@icloud.com>
2016-05-07 21:39:25 +0200

    Pendiente selector de canción con un UIAlertController y meter las canciones

--- Reproductor de Música/ViewController.swift
+++ Reproductor de Música/ViewController.swift
@@ -14,17 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var controlVolumen: UIStepper!
     @IBOutlet weak var portada: UIImageView!
     @IBOutlet weak var título: UILabel!
+    @IBOutlet weak var sliderVolumen: UISlider!
     
-    var reproductor = AVAudioPlayer()
+    @IBOutlet weak var botónDetener: UIButton!
+    @IBOutlet weak var botónTocar: UIButton!
+    @IBOutlet weak var botónPausar: UIButton!
+    
+    var reproductor: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(reproductor.volume)
-        /*if let volumen = reproductor.volume {
-            controlVolumen.value = Double(volumen)
-        }*/
+        let path = NSBundle.mainBundle().pathForResource("137 horas.mp3", ofType:nil)!
+        let url = NSURL(fileURLWithPath: path)
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOfURL: url)
+            reproductor = sound
+            
+            controlVolumen.value = Double(reproductor.volume)
+            sliderVolumen.value = reproductor.volume
+        } catch {
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,27 +46,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func canciónAleatoria(sender: UIBarButtonItem) {
+    @IBAction func canciónAleatoria() {
     }
     
-    @IBAction func buscarCanción(sender: UIBarButtonItem) {
+    @IBAction func buscarCanción() {
     }
 
     @IBAction func deneterReproducción() {
+        reproductor.stop()
+        reproductor.currentTime = 0
     }
 
     @IBAction func iniciarCanción() {
+        reproductor.play()
+        
     }
     
     @IBAction func pausarReproducción() {
+        reproductor.pause()
     }
     
     @IBAction func cambioDeVolumen(sender: UIStepper) {
-        print(sender.value)
-        
-        /*if let volumen = reproductor.volume {
-            volumen = Float(sender.value)
-        }*/
+        reproductor.volume = Float(sender.value)
+        sliderVolumen.value = reproductor.volume
     }
+    
+    @IBAction func destacarBotónPulsado(sender: UIButton) {
+        let bold = UIFont.boldSystemFontOfSize(15)
+        let regular = UIFont.systemFontOfSize(15)
+        
+        switch sender {
+        case botónDetener:
+            botónTocar.titleLabel?.font = regular
+            botónPausar.titleLabel?.font = regular
+            break
+        case botónTocar:
+            botónTocar.titleLabel?.font = bold
+            botónPausar.titleLabel?.font = regular
+            break
+        case botónPausar:
+            botónTocar.titleLabel?.font = regular
+            botónPausar.titleLabel?.font = bold
+            break
+        default:
+            break
+        }
+    }
+    
 }
 
