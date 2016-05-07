da7d6308fc2d22a78dc9b9d26cdec94f64bacdc4
Arturo Rivas <arturo.rivas@icloud.com>
2016-05-07 09:09:34 +0200

    Initial Commit

---
+++ Reproductor de Música/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reproductor de Música
+//
+//  Created by Arturo Rivas on 7/5/16.
+//  Copyright © 2016 Arturo Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
